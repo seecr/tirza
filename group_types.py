893f4a8c9bcb4e6afd6145e049be3d32efd06178
Maximilian Konzack <mkonzack@acm.org>
2013-11-13 15:16:47 +0100

    skeleton for group_types

---
+++ group_types.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+import numpy as np
+
+def read_type_mapping():
+    pass # TODO
+
+def read_sim_matrix():
+    pass # TODO
+
+def calculate_groups():
+    pass # TODO
+
+def calcluate_similarity():
+    pass # TODO
+
+def output_csv():
+    pass # TODO
+
+def main():
+    pass
+
+if __name__ == '__main__':
+    main()
