4be03fd426370ff04cc49ec8a95c68a6e15fad86
Maximilian Konzack <mkonzack@acm.org>
2013-11-13 15:59:25 +0100

    some impl

--- group_types.py
+++ group_types.py
@@ -1,9 +1,16 @@
 #!/usr/bin/env python
 
 import numpy as np
+import csv
 
 def read_type_mapping():
-    pass # TODO
+    group_mapping = {}
+    with open('tirza_uri_type.csv', 'r') as csv_file:
+        csv_reader = csv.reader(csv_file, delimiter=',')
+        for row in csv_reader:
+            group_mapping[row[0]] = row[1]
+    print group_mapping
+    return group_mapping
 
 def read_sim_matrix():
     pass # TODO
@@ -18,7 +25,7 @@ def output_csv():
     pass # TODO
 
 def main():
-    pass
+    read_type_mapping()
 
 if __name__ == '__main__':
     main()
